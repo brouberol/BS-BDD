d8ac9e5f6d3fe3dbcc7179ed0a7d26e94d63d9c8
Balthazar Rouberol <rouberol.b@gmail.com>
2012-01-06 12:04:40 +0100

    url & settings config
    Signed-off-by: Balthazar Rouberol <rouberol.b@gmail.com>

--- settings.py
+++ settings.py
@@ -119,6 +119,9 @@ INSTALLED_APPS = (
     'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     'django.contrib.admindocs',
+    'eleve',
+    'admission',
+    'etat_civil',
 )
 
 # A sample logging configuration. The only tangible logging
