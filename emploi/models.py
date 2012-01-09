9473d30fb0a6e28061d5321ee11bb6a1ce30d815
Balthazar Rouberol <rouberol.b@gmail.com>
2012-01-09 10:38:54 +0100

    Emploi model update : need for entreprise model
    Signed-off-by: Balthazar Rouberol <rouberol.b@gmail.com>

--- emploi/models.py
+++ emploi/models.py
@@ -1,3 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Model describing job titles chosen by former student
+"""
+
 from django.db import models
 
-# Create your models here.
+from entreprise.models import Entreprise
+
+class Emploi(models.Model):
+    """Job title description"""
+    
+    titre      = models.CharField(max_length=75, verbose_name=u'Description du poste')
+    entreprise = models.ForeignKey(Entreprise)
+    
