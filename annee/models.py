002c3c43836dbde97ed603d39ac47f689097a0f3
Balthazar Rouberol <rouberol.b@gmail.com>
2012-01-12 09:40:52 +0100

    annee.models update
    Signed-off-by: Balthazar Rouberol <rouberol.b@gmail.com>

--- annee/models.py
+++ annee/models.py
@@ -4,7 +4,9 @@ Model defining different types of scholar year
 """
 
 from django.db import models
-from univ.models import Universite
+
+from BS.eleve.models import Eleve
+from BS.international.models import Universite
 
 class AnneeBS(models.Model):
     """A traditional BS scholar year"""
@@ -21,44 +23,45 @@ class AnneeBS(models.Model):
     filiere      = models.CharField(max_length=5, choices=FILIERE, verbose_name=u'Filière', help_text=u"Bioch : filière avant 199X") # TODO : update help_text
     num_promo    = models.PositiveIntegerField(verbose_name=u'Numéro de la promotion')
     taille_promo = models.PositiveIntegerField(verbose_name=u'Taille de la promotion')
-    # TODO ; add date  (20XX - 20XX)
+    annee        = models.PositiveIntegerField(help_text=u"Indiquer l'année de début de l'année scolaire (ex: 2011 pour 2011-2012)")
 
     def __unicode__(self):
         """ AnneeBS instance representation """
         if self.categorie not in ['3', '4', '5']: # cesure / sabbatique
-            return '%s - %s - Promo %d' %(self.filiere, self.categorie, self.num_promo)
+            return '%s - %s - %d - Promo %d' %(self.filiere, self.categorie, self.annee, self.num_promo)
         else:
-            return '%s%s - Promo %d' %(self.categorie, self.filiere, self.num_promo)
+            return '%s%s - %d - Promo %d' %(self.categorie, self.filiere, self.annee, self.num_promo)
 
     class Meta:
         verbose_name = u"Année de scolarité BS"
         verbose_name_plural = u"Années de scolarité BS"
         unique_together = [('categorie', 'filiere', 'num_promo')]
 
-class Echange(models.Model):
-    """Academic exchange"""
-    
-    universite = models.ForeignKey(Universite, verbose_name=u"Université d'accueil")
-    duree      = models.PositiveSmallIntegerField(verbose_name=u"Durée de l'échange", help_text=u"En mois. Donnez un nombre rond.")
-
-    def __unicode__(self):
-        return '%s - %s - %d mois' %(self.universite.pays, self.universite.nom, self.duree)
-    
-
-class EchangeEleve(models.Model):
-    """Instance of the relation student <-> academic exchange"""
-    
-    from eleve.models import Eleve
+class AnneeBSEleve(models.Model):
+    """Association between a student and a BS scholar year"""
     
     eleve    = models.ForeignKey(Eleve)
     annee_bs = models.ForeignKey(AnneeBS)
-    echange  = models.ForeignKey(Echange)
+
+
+class EchangeEleve(models.Model):
+    """Association between an academic exchange and a student's scholar year"""
     
+    universite  = models.ForeignKey(Universite, verbose_name=u"Université d'accueil")
+    annee_eleve = models.ForeignKey(AnneeBSEleve, verbose_name=u"Année d'échange") 
+    duree       = models.PositiveSmallIntegerField(verbose_name=u"Durée de l'échange", help_text=u"En mois. Donnez un nombre rond.")
+
     def __unicode__(self):
-        return '%s - %s' %(self.annee_bs, self.echange)
-    
+        return '%s - %s - %s - %d mois' %(self.annee_eleve, self.universite.pays, self.universite.nom, self.duree)
+
     class Meta:
         verbose_name =u"Élève parti en échange académique"
         verbose_name_plural = u"Élèves partis en échange académique"
 
 
+class ResultatEleve(models.Model):
+    """Association between a student's scholar year and his/her results"""
+
+    annee_eleve = models.ForeignKey(AnneeBSEleve, verbose_name=u"Élève & année scolaire")
+    rang        = models.PositiveIntegerField(verbose_name=u"Classement")
+   
