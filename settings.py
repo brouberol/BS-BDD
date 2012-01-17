3fadb89083d4056843bc2a8b9c8ec34a4f7e6c85
Balthazar Rouberol <rouberol.b@gmail.com>
2012-01-17 13:58:32 +0100

    Template directory
    Signed-off-by: Balthazar Rouberol <rouberol.b@gmail.com>

--- settings.py
+++ settings.py
@@ -106,7 +106,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    '/home/balto/Projects/BS_BDD/templates/',
+    '/home/balto/Projects/BS_BDD/BS/templates/',
 )
 
 INSTALLED_APPS = (
