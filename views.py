7f0587b9050fdedfd3768b3d2e8966f1900c02c4
Balthazar Rouberol <rouberol.b@gmail.com>
2012-01-18 09:13:47 +0100

    generic views
    Signed-off-by: Balthazar Rouberol <rouberol.b@gmail.com>

---
+++ views.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Site-level views (home page, excel input, ...)
+"""
+
+from django.shortcuts import render_to_response
+from django.template import RequestContext
+
+    
