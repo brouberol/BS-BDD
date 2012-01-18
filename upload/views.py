a46ec931d2860692e13830a828c574cc7c962aa9
Balthazar Rouberol <rouberol.b@gmail.com>
2012-01-18 16:43:11 +0100

    New upload.views : admission & user creation & checking
    Signed-off-by: Balthazar Rouberol <rouberol.b@gmail.com>

--- upload/views.py
+++ upload/views.py
@@ -8,10 +8,13 @@ from django.shortcuts import get_object_or_404, render_to_response
 from django.http import Http404, HttpResponseRedirect
 from django.db import transaction
 from django.core.exceptions import ValidationError
+from django.contrib.auth.models import User
 
 from os.path import exists
 from os import makedirs
 from datetime import datetime
+from random import choice
+import string
 
 from upload.forms import DataInputForm
 from excel.headers import *
@@ -20,6 +23,7 @@ from BS.settings import MEDIA_ROOT
 
 from etat_civil.models import EtatCivil
 from international.models import Pays
+from admission.models import Admission, FiliereAdmission, DomaineAdmission
 
 @transaction.commit_on_success
 def upload(request):
@@ -54,9 +58,7 @@ def save_excel(file):
 
 def create_etat_civil(row):
     """Read an excel row and save the corresponding EtatCivil object in database"""
-
     errors = []
-
     nationalite, created = Pays.objects.get_or_create(nom=unicode(row['nationalite']))            
     ec = EtatCivil()
     ec.nom_insa = row['nom_insa']
@@ -72,18 +74,45 @@ def create_etat_civil(row):
     ec.zip_adresse_2 = row['code_postal_2']
     ec.email_1 = row['email_1']
     ec.email_2 = row['email_2']
-    
     try:
         ec.full_clean()
     except ValidationError, e:
         m = dict(e.message_dict)
         errors.append([str("%s %s"%(ec.prenom, ec.nom_insa)), [list(k) for k in zip(m.keys(),m.values())]])
-        print errors
 
     return ec, errors 
             
+def create_user(etat_civil):
+    """Create user associated with an etat civil"""
+    errors = []
+    us = User()
+    us.username = etat_civil.prenom[0].lower() + etat_civil.nom_insa.lower()
+    chars = string.ascii_letters + string.digits
+    us.password = "".join(choice(chars) for x in range(8))
+    try:
+        us.full_clean()
+    except ValidationError, e:
+        m = dict(e.message_dict)
+        errors.append([str("%s %s"%(etat_civil.prenom, etat_civil.nom_insa)), [list(k) for k in zip(m.keys(),m.values())]])  
+    return us, errors                      
     
+def create_admission(row, etat_civil):
+    """Read an excel row and save the corresponding Admission object in database"""
+    errors = []
+    ad = Admission()
+    ad.filiere_org = FiliereAdmission.objects.get_or_create(nom=row['origine_avant_BIM'])[0]
+    ad.domaine_org = DomaineAdmission.objects.get_or_create(nom=row['filiere_avant_BIM'])[0]
+    ad.annee_admission = row['annee_admission']
+    ad.rang_pre_BS = row['classement_avant_BIM']
+    ad.taille_promo_pre_BS = row['taille_de_promo_avant_BIM']
+    try:
+        ad.full_clean()
+    except ValidationError, e:
+        m = dict(e.message_dict)
+        errors.append([str("%s %s"%(etat_civil.prenom, etat_civil.nom_insa)), [list(k) for k in zip(m.keys(),m.values())]])  
+    return ad, errors                      
     
+ 
 def populate_database(file):
     """
     Save the input excel file in the /MEDIA_ROOT/upload/ directory,
@@ -96,11 +125,21 @@ def populate_database(file):
     errors = []
     upload = True # If upload still True at the end, --> save
 
-    for nrow in range (1, rowmax+1):
+    for nrow in range (1, rowmax+1): 
         row = xl.get_row(sheet, nrow)
         
-        ec, errors = create_etat_civil(row)
+        # ETAT CIVIL
+        ec, e = create_etat_civil(row)
+        if len(e) > 0:errors.append(e)
             
+        # USER
+        us, e = create_user(ec)    
+        if len(e) > 0:errors.append(e)
+        
+        # ADMISSION
+        ad, e = create_admission(row, ec)
+        if len(e) > 0:errors.append(e)
+        
     if len(errors)==0:
         upload = False
     else:
